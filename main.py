710f581f435c75c6643bf3e71147738c1b95c685
PareekshithPalat <paripalat@gmail.com>
2025-12-31 22:01:13 +0530

    Changed run method

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def health():
 
 @app.post("/prompt")
 def run_agent(data: Prompt):
-    result = root_agent.run(data.prompt)
+    result = root_agent.run_live(data.prompt)
     return {"response": result}
